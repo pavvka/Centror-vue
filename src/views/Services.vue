8bd6e92e12ff708641a1e45c5eab40d58909c746
mark Тамаров <googlcraftlol@gmail.com>
2020-02-28 01:21:28 +0300

    working on search

---
+++ src/views/Services.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <ServicesComponents> </ServicesComponents>
+  </div>
+</template>
+
+
+<script>
+import ServicesComponents from '../components/ServicesComponents'
+
+export default {
+  name: 'app',
+    data(){
+    return {
+    }
+  },
+  components: {
+    ServicesComponents
+  },
+  methods: {
+
+  },
+    beforeMount(){
+      
+ },
+}
+</script>
+
+<style>
+
+</style>
