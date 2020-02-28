024ed94b9878aaf0230183b7a98caa3c9080f25e
Pasha <prostobux@yandex.ru>
2020-02-29 00:23:38 +0300

    carusel done (need adapt)

--- src/components/mainPrograms.vue
+++ src/components/mainPrograms.vue
@@ -1,61 +1,213 @@
 <template>
     <section class="ourPrograms">
         <div class="container">
-            <div class="row">
-                <div class="col-lg-4" v-for="(item,index) in 3" :key="index">
-                    <div class="program">
-                        <h1 class="program__title">Избранная программа</h1>
-                        <div class="program__imgBlock"></div>
-                        <div class="program__text">
-                            Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация
-                            Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация
-                            Краткая аннотация
-                        </div>
-                    </div>
-                </div>
+            <div class="card-carousel-wrapper">
+    <div class="card-carousel--nav__left" @click="moveCarousel(-1)" :disabled="atHeadOfList"></div>
+    <div class="card-carousel">
+      <div class="card-carousel--overflow-container">
+        <div class="card-carousel-cards" :style="{ transform: 'translateX' + '(' + currentOffset + 'px' + ')'}">
+          <div class="card-carousel--card" v-for="(item,index) in items" :key="index"><img src="https://placehold.it/350x180"/>
+            <div class="card-carousel--card--footer">
+              <p class="program__title">{{ item.name }}</p>
+              <p class="program__text">{{ item.text }}</p>
             </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card-carousel--nav__right" @click="moveCarousel(1)" :disabled="atEndOfList"></div>
+  </div>
         </div>
     </section>
+
+
 </template>
 
 <script>
+import Slick from 'vue-slick';
 export default {
   name: 'app',
   data () {
     return {
-
+    currentOffset: 0,
+      windowSize: 3,
+      paginationFactor: 390,
+      items: [
+        {name: 'Kin Khao', text:"Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация"},
+        {name: 'Jū-Ni', text:"Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация"},
+        {name: 'Delfina', text:"Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация"},
+        {name: 'San Tung', text:"Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация"},
+        {name: 'Anchor Oyster Bar', text:"Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация"},
+        {name: 'Locanda', text:"Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация"},
+        {name: 'Garden Creamery', text:"Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация Краткая аннотация"},
+      ]
     }
+  },
+    computed: {
+    atEndOfList() {
+      return this.currentOffset <= (this.paginationFactor * -1) * (this.items.length - this.windowSize);
+    },
+    atHeadOfList() {
+      return this.currentOffset === 0;
+    },
+  },
+  methods: {
+    moveCarousel(direction) {
+      // Find a more elegant way to express the :style. consider using props to make it truly generic
+      if (direction === 1 && !this.atEndOfList) {
+        this.currentOffset -= this.paginationFactor;
+      } else if (direction === -1 && !this.atHeadOfList) {
+        this.currentOffset += this.paginationFactor;
+      }
+    },
   }
 }
 </script>
 
 <style scoped>
-.program{
-    font-family: 'Montserrat', sans-serif;
-    color: #fff;
-    background-color: rgba(97, 139, 170, 0.44);
-    width: 350px;
-    text-align: center;
-    margin-bottom: 30px;
-    border-radius: 8px;
-    padding: 8px;
-}
-.program__imgBlock{
-    width: 310px;
-    height: 150px;
-    background: grey;
-    display: inline-block;
-}
-.program__title{
-    font-size: 15px;
-    padding: 5px;
-    padding-top: 10px;
-}
+
+
 .program__text{
-    width: 280px;
+    width: 100%;
     font-size: 15px;
     display: inline-block;
     padding-bottom: 10px;
     padding-top: 10px;
 }
+
+
+.card-carousel-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 20px 0 40px;
+  color: #666a73;
+}
+
+.card-carousel {
+  display: flex;
+  justify-content: center;
+  width: 1150px;
+}
+.card-carousel--overflow-container {
+  overflow: hidden;
+}
+.card-carousel--nav__left, .card-carousel--nav__right {
+  display: inline-block;
+  width: 15px;
+  height: 15px;
+  padding: 10px;
+  box-sizing: border-box;
+  border-top: 2px solid #42b883;
+  border-right: 2px solid #42b883;
+  cursor: pointer;
+  margin: 0 20px;
+  transition: transform 150ms linear;
+}
+.card-carousel--nav__left[disabled], .card-carousel--nav__right[disabled] {
+  opacity: 0.2;
+  border-color: black;
+}
+.card-carousel--nav__left {
+  transform: rotate(-135deg);
+}
+.card-carousel--nav__left:active {
+  transform: rotate(-135deg) scale(0.9);
+}
+.card-carousel--nav__right {
+  transform: rotate(45deg);
+}
+.card-carousel--nav__right:active {
+  transform: rotate(45deg) scale(0.9);
+}
+
+.card-carousel-cards {
+  display: flex;
+  transition: transform 150ms ease-out;
+  transform: translatex(0px);
+}
+.card-carousel-cards .card-carousel--card {
+  margin: 0 20px;
+  width: 350px;
+  cursor: pointer;
+  box-shadow: 0 4px 15px 0 rgba(40, 44, 53, 0.06), 0 2px 2px 0 rgba(40, 44, 53, 0.08);
+  font-family: 'Montserrat', sans-serif;
+  background-color: rgba(97, 139, 170, 0.44);
+  border-radius: 4px;
+  z-index: 3;
+  margin-bottom: 2px;
+}
+.card-carousel-cards .card-carousel--card:first-child {
+  margin-left: 0;
+}
+.card-carousel-cards .card-carousel--card:last-child {
+  margin-right: 0;
+}
+.card-carousel-cards .card-carousel--card img {
+  vertical-align: bottom;
+  border-top-left-radius: 4px;
+  border-top-right-radius: 4px;
+  transition: opacity 150ms linear;
+  user-select: none;
+}
+.card-carousel-cards .card-carousel--card img:hover {
+  opacity: 0.5;
+}
+.card-carousel-cards .card-carousel--card--footer {
+  border-top: 0;
+  padding: 7px 15px;
+}
+.card-carousel-cards .card-carousel--card--footer p {
+  padding: 3px 0;
+  margin: 0;
+  margin-bottom: 2px;
+  font-size: 15px;
+  font-weight: 500;
+  color: #fff;
+  text-align: center;
+  user-select: none;
+}
+.card-carousel-cards .card-carousel--card--footer p.tag {
+  font-size: 11px;
+  font-weight: 300;
+  padding: 4px;
+  background: rgba(40, 44, 53, 0.06);
+  display: inline-block;
+  position: relative;
+  margin-left: 4px;
+}
+.card-carousel-cards .card-carousel--card--footer p.tag:before {
+  content: "";
+  float: left;
+  position: absolute;
+  top: 0;
+  left: -12px;
+  width: 0;
+  height: 0;
+  border-color: transparent rgba(40, 44, 53, 0.06) transparent transparent;
+  border-style: solid;
+  border-width: 8px 12px 12px 0;
+}
+.card-carousel-cards .card-carousel--card--footer p.tag.secondary {
+  margin-left: 0;
+  border-left: 1.45px dashed white;
+}
+.card-carousel-cards .card-carousel--card--footer p.tag.secondary:before {
+  display: none !important;
+}
+.card-carousel-cards .card-carousel--card--footer p.tag:after {
+  content: "";
+  position: absolute;
+  top: 8px;
+  left: -3px;
+  float: left;
+  width: 4px;
+  height: 4px;
+  border-radius: 2px;
+  background: white;
+  box-shadow: -0px -0px 0px #004977;
+}
+
+
+
 </style>
