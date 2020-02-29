69e406bf5c711dddbd1aabfc914fd1dd9943be16
Pasha <prostobux@yandex.ru>
2020-03-01 01:39:58 +0300

    about done

--- src/views/About.vue
+++ src/views/About.vue
@@ -24,10 +24,10 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                    <div class="teamMembers">
+                    <div class="Stories">
                         <div class="row">
                             <div class="col-lg-4">
-                                <h2 class="teamMembers__title">
+                                <h2 class="Stories__title">
                                     Блок истории
                                 </h2>
                             </div>
@@ -50,8 +50,8 @@
                               </p>
                             </div>
                             <div class="col-lg-4">
-                                <div class="teamMembers__person">
-                                    <img src="../images/member2.png" alt="" class="teamMembers__person__img">
+                                <div class="Stories__person">
+                                    <img src="../images/member2.png" alt="" class="Stories__person__img">
                                 </div>
                             </div>
                         </div>
@@ -59,34 +59,25 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-lg-12">
-                    <div class="Portfolio">
-                        <div class="row">
-                            <div class="col-lg-4">
-                                <h2 class="Portfolio__title">
-                                    Портфолио
-                                </h2>
-                            </div>
-                        </div>
-                        <div class="row company" v-for="(index,i) in 3" :key="i">
-                            <div class="col-lg-1"></div>
-                            <div class="col-lg-4">
-                                <h3 class="company__title">
-                                    Company
-                                </h3>
-                                <div class="company__text">
-                                    description omg description omg description omg description omg
-                                    description omg description omg description omg description omg
-                                </div>
-                            </div>
-                            <div class="col-lg-2"></div>
-                            <div class="col-lg-4">
-                                <img src="../images/study1.png" alt="" class="company__img">
-                            </div>
-                            <div class="col-lg-1"></div>
-                        </div>
-                    </div>
+              <div class="col-lg-12">
+                <div class="teamMembers__title__block">
+                  <h2 class="teamMembers__title">Наша Команда</h2>
                 </div>
+              </div>
+              
+            </div>
+            <div class="row">
+              <div class="col-lg-4" v-for="(member,index) in 6" :key="index">
+                <div class="member">
+                    <div class="member__img">
+                      <img src="../images/member2.png" alt="" class="member__pict">
+                    </div>
+                    <h1 class="member__fio">ФИО</h1>
+                    <p class="member__status">
+                      Должность
+                    </p>
+                </div>
+              </div>
             </div>
         </div>
     </section>
@@ -128,7 +119,7 @@ export default {
     margin-top: 30px;
     margin-bottom: 80px;
 }
-.teamMembers{
+.Stories{
     color: #fff;
     min-height: 500px;
     background: linear-gradient(0deg, #2C3846, #2C3846), linear-gradient(179.72deg, rgba(97, 139, 170, 0.44) 7.58%, rgba(58, 71, 90, 0) 99.54%);
@@ -136,14 +127,15 @@ export default {
     margin-top: 100px;
     padding: 10px;
 }
-.teamMembers__title{
+.Stories__title{
+  color: #fff;
     font-size: 50px;
     margin-top: 30px;
 }
-.teamMembers__person{
+.Stories__person{
     margin-top: 30px;
 }
-.teamMembers__person__img{
+.Stories__person__img{
     width: 250px;
     margin-bottom: 20px;
 }
@@ -173,6 +165,36 @@ export default {
 }
 .company__img{
     width: 250px;
+}
+
+/* Members */
+.teamMembers__title__block{
+  text-align: center;
+  margin-top: 40px;
+  margin-bottom: 30px;
+}
+.teamMembers__title{
+  color: #fff;
+}
+.member{
+  max-width: 350px;
+  color: #fff;
+  border-radius: 10px;
+  background: #618BAA;
+  text-align: center;
+  min-height: 200px;
+  padding: 10px;
+  margin-bottom: 30px;
+}
+.member__pict{
+  border-radius: 170px;
+  width: 130px;
+}
+.member__fio{
+  display: inline-block;
+  width: 200px;
+  border-bottom: 1px #fff solid;
+  padding-bottom: 8px;
 }
 </style>
 
