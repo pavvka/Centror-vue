93cf7814bf859e5df01c1e555792a3adc0b05934
Pasha <prostobux@yandex.ru>
2020-03-01 02:21:36 +0300

    one block created

--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -1,5 +1,81 @@
 <template>
     <section class="ourContacts">
-        
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="contacts__block">
+                        <div class="row">
+                            <div class="col-lg-4">
+                                <div class="contact__peace">
+                                    <span class="icon">
+                                        <i class="person__icon fa fa-phone-square"></i>
+                                    </span>
+                                    <h2 class="title">Позвоните нам</h2>
+                                    <div class="firstPhrase">Есть вопросы?</div>
+                                    <div class="secondPhrase">Мы поможем!</div>
+                                    <h3 class="number">8 666 555 33 22</h3>
+                                    <a class="siteCall">Позвонить с сайта</a>
+                                </div>
+                            </div>
+                            <div class="col-lg-4">
+                                <div class="contact__peace">
+                                    <span class="icon">
+                                        <i class="person__icon fa fa-phone-square"></i>
+                                    </span>
+                                    <h2 class="title">Позвоните нам</h2>
+                                    <div class="firstPhrase">Есть вопросы?</div>
+                                    <div class="secondPhrase">Мы поможем!</div>
+                                    <h3 class="number">8 666 555 33 22</h3>
+                                    <a class="siteCall">Позвонить с сайта</a>
+                                </div>
+                            </div>
+                            <div class="col-lg-4">
+                                <div class="contact__peace">
+                                    <span class="icon">
+                                        <i class="person__icon fa fa-phone-square"></i>
+                                    </span>
+                                    <h2 class="title">Позвоните нам</h2>
+                                    <div class="firstPhrase">Есть вопросы?</div>
+                                    <div class="secondPhrase">Мы поможем!</div>
+                                    <h3 class="number">8 666 555 33 22</h3>
+                                    <a class="siteCall">Позвонить с сайта</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
 </template>
+
+<style scoped>
+.contacts__block{
+    min-height: 400px;
+    background: linear-gradient(0deg, #2C3846, #2C3846), linear-gradient(179.82deg, rgba(97, 139, 170, 0.44) 7.58%, rgba(58, 71, 90, 0) 99.54%);
+    border-radius: 10px;
+}
+.contact__peace{
+    margin-top: 60px;
+    max-width: 350px;
+    text-align: center;
+    color: #fff;
+}
+.icon{
+    font-size: 60px;
+}
+.title{
+    font-size: 25px;
+}
+.firstPhrase, .secondPhrase{
+    font-size: 18px;
+    font-family: Montserrat
+}
+.secondPhrase{
+    margin-bottom: 10px;
+}
+.number{
+    margin-bottom: 30px;
+    font-family: Teko;
+}
+</style>
