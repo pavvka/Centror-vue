07de90702d4924ade7b678b08b456663362473b3
Pasha <prostobux@yandex.ru>
2020-03-04 11:03:02 +0300

    cases in work

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Home from '@/views/Home.vue'
 import TrainingList from '@/views/TrainingList.vue'
 import Training from '@/views/Training.vue'
 import Contacts from '@/views/Contacts.vue'
+import Cases from '@/views/Cases.vue'
 
 Vue.use(VueRouter)
 
@@ -48,6 +49,11 @@ const routes = [
     path: '/contacts',
     name: 'contacts',
     component: Contacts
+  },
+  {
+    path: '/cases',
+    name: 'cases',
+    component: Cases
   }
 ]
 
---
+++ src/views/Cases.vue
@@ -0,0 +1,20 @@
+<template>
+    <section class="ourParallax">
+        
+    </section>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
